1c7350e37c00a714e3ee07450b13d755ebac4318
Mikołaj Lubiak <lubiak@proton.me>
2024-07-05 20:13:27 +0200

    Initial commit

---
+++ aspdotnet/Controllers/AutomobilesController.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using aspdotnet.Data;
+using aspdotnet.Models;
+
+namespace aspdotnet.Controllers
+{
+    public class AutomobilesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public AutomobilesController(ApplicationDbContext context, UserManager<IdentityUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        // GET: Autoes/Search
+        public IActionResult Search()
+        {
+            return View();
+        }
+
+        // POST: Autoes/SearchResults
+        public async Task<IActionResult> SearchResults(String SearchPhrase)
+        {
+            return View("Index", await _context.Auto.Where(a => a.Nazwa.Contains(SearchPhrase)).ToListAsync());
+        }
+
+        // GET: Autoes/OwnedCars
+        [Authorize]
+        public async Task<IActionResult> OwnedCars()
+        {
+            var currentUser = await _userManager.GetUserAsync(User);
+            if (currentUser != null)
+            {
+                return View(await _context.Auto.Where(a => a.Email.Equals(currentUser.Email)).ToListAsync());
+            }
+            return View();
+        }
+
+        // GET: Autoes/Buy
+        public async Task<IActionResult> Buy(int? id)
+		{
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var auto = await _context.Auto.FindAsync(id);
+            if (auto == null)
+            {
+                return NotFound();
+            }
+            return View(auto);
+        }
+
+        /*
+        // POST: Autoes/BuyCar/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize]
+        public async Task<IActionResult> BuyCar(int id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var auto = await _context.Automobile.FindAsync(id);
+            if (auto == null)
+            {
+                return NotFound();
+            }
+
+            var currentUser = await _userManager.GetUserAsync(User);
+            if (currentUser == null)
+            {
+                return NotFound();
+            }
+
+            auto.Email = currentUser.Email;
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(auto);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AutoExists(auto.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(auto);
+        }
+        */
+
+        // POST: Autoes/BuyCar/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize]
+        public async Task<IActionResult> BuyCar(int id, [Bind("Id,Cena,Nazwa,Imie_Nazwisko,Email,Odbior,Oddanie")] Automobile auto)
+        {
+            if (id != auto.Id)
+            {
+                return NotFound();
+            }
+
+			var currentUser = await _userManager.GetUserAsync(User);
+			if (currentUser == null)
+			{
+				return NotFound();
+			}
+
+			auto.Email = currentUser.Email;
+
+			if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(auto);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AutoExists(auto.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(auto);
+        }
+
+        // GET: Autoes
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Auto.ToListAsync());
+        }
+
+        // GET: Autoes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var auto = await _context.Auto
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (auto == null)
+            {
+                return NotFound();
+            }
+
+            return View(auto);
+        }
+
+        // GET: Autoes/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Autoes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize]
+        public async Task<IActionResult> Create([Bind("Id,Cena,Nazwa,Imie_Nazwisko,Email,Odbior,Oddanie")] Automobile auto)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(auto);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(auto);
+        }
+
+        // GET: Autoes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var auto = await _context.Auto.FindAsync(id);
+            if (auto == null)
+            {
+                return NotFound();
+            }
+            return View(auto);
+        }
+
+        // POST: Autoes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Cena,Nazwa,Imie_Nazwisko,Email,Odbior,Oddanie")] Automobile auto)
+        {
+            if (id != auto.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(auto);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AutoExists(auto.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(auto);
+        }
+
+        // GET: Autoes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var auto = await _context.Auto
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (auto == null)
+            {
+                return NotFound();
+            }
+
+            return View(auto);
+        }
+
+        // POST: Autoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        [Authorize]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var auto = await _context.Auto.FindAsync(id);
+            if (auto != null)
+            {
+                _context.Auto.Remove(auto);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AutoExists(int id)
+        {
+            return _context.Auto.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ aspdotnet/Models/Automobile.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace aspdotnet.Models
+{
+    public class Automobile
+    {
+        public int Id { get; set; }
+        public int Cena { get; set; }
+        public string Nazwa { get; set; }
+
+        // Wlasciciel
+        public string? Imie_Nazwisko { get; set; }
+        public string? Email { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime? Odbior { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime? Oddanie { get; set; }
+
+        // Methods
+        public Automobile()
+        {
+        }
+    }
+}
